26f5cab6c07515e639c04e1cf3de1754deb7aa63
Devansh Aryan <devansh.aryan@sprinklr.com>
2024-03-22 15:25:28 +0530

    renamed variables

--- src/components/chatContainer/ChatContainer.tsx
+++ src/components/chatContainer/ChatContainer.tsx
@@ -6,10 +6,10 @@ import { Box } from "@sprinklrjs/spaceweb/box";
 import { Header } from "./components/header";
 import { Body } from "./components/body";
 
-const cssClass=[{backgroundColor:"#EBEBEB"}]; // className
+const className=[{backgroundColor:"#EBEBEB"}]; // className
 export const ChatContainer = (): ReactElement => (
   <Box className="flex flex-col">
-    <Header className={cssClass} />
+    <Header className={className} />
     <Body />
   </Box>
 );
